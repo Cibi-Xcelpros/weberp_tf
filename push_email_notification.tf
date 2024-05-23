6963a29d51dc7e985bc14fbae956826d15e1d178
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-23 17:48:47 +0530

    Create push_email_notification.tf

---
+++ push_email_notification.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "send_email" {
+  depends_on = [null_resource.set_infra_expiry]
+
+  provisioner "local-exec" {
+    command = <<EOT
+        curl -X POST "http://${var.email_service_ip}:2222/sendemail?api_key=${var.api_key}&tenant_name=${var.tenant_name}&subdomin=${var.sub_domain_name}&git_branch=${var.git_branch}&to_email_id=${var.email_id_notification}"
+      EOT
+  }
+}
