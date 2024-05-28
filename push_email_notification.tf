11b83549f3255667b8161df489e1c702a00c02db
Cibi-Xcelpros <165359840+Cibi-Xcelpros@users.noreply.github.com>
2024-05-28 14:42:21 +0530

    Update push_email_notification.tf

--- push_email_notification.tf
+++ push_email_notification.tf
@@ -3,7 +3,7 @@ resource "null_resource" "send_email" {
 
   provisioner "local-exec" {
     command = <<EOT
-        curl -X POST "http://${var.email_service_ip}:2222/sendemail?api_key=${var.api_key}&tenant_name=${var.tenant_name}&subdomin=${var.sub_domain_name}&git_branch=${var.git_branch}&to_email_id=${var.email_id_notification}"
+        curl -X POST "http://${var.email_service_ip}:2222/newtenant?api_key=${var.api_key}&tenant_name=${var.tenant_name}&subdomin=${var.sub_domain_name}&git_branch=${var.git_branch}&to_email_id=${var.email_id_notification}"
       EOT
   }
 }
